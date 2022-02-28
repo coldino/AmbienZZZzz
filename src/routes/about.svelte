530592724933d0f0968198314d653b40e97dbf26
coldino <28345893+coldino@users.noreply.github.com>
2022-02-28 15:44:08 +0000

    feat: add author information

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -11,8 +11,8 @@
 		<h1 class="text-4xl mb-8 font-bold">AmbienZZZzz</h1>
 		<p>
 			AmbienZZZzz is an easy to use ambient sound mixer. No matter if you want to focus on your work
-			or fall asleep. It is proven that certain sounds help you gain focus by raising the
-			hearing threshold when constantly playing.
+			or fall asleep. It is proven that certain sounds help you gain focus by raising the hearing
+			threshold when constantly playing.
 		</p>
 		<br />
 		<p>
@@ -28,7 +28,30 @@
 	</div>
 
 	<div>
-		<p>This app uses sounds licensed under the Creative Commons:</p>
+		<p>
+			This app was made by <a href="https://github.com/coldino" target="_blank">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 12.7 12.7"
+					class="w-6 inline ml-1 pb-6px"
+				>
+					<path
+						fill="#fff"
+						fill-rule="evenodd"
+						d="M6.35.3a6.15 6.15 0 0 0-1.94 12c.3.05.42-.14.42-.3l-.01-1.04c-1.71.37-2.07-.83-2.07-.83-.28-.7-.68-.9-.68-.9-.56-.38.04-.37.04-.37.62.04.94.63.94.63.55.94 1.44.67 1.79.51.06-.4.21-.66.39-.82-1.37-.15-2.8-.68-2.8-3.04 0-.67.24-1.22.63-1.65-.06-.15-.27-.78.06-1.62 0 0 .52-.17 1.7.63a5.87 5.87 0 0 1 3.07 0c1.18-.8 1.7-.63 1.7-.63.33.84.12 1.47.05 1.62.4.43.63.98.63 1.65 0 2.36-1.43 2.88-2.8 3.04.22.19.41.56.41 1.14V12c0 .17.1.36.42.3A6.15 6.15 0 0 0 6.35.3"
+					/>
+				</svg>
+				Coldino</a
+			> for <a href="https://thecodingchannel.hashnode.dev/">The Coding Channel</a>.
+		</p>
+		<p>
+			It is developed on the <a href="https://kit.svelte.dev/">SvelteKit</a> framework and is hosted on
+			<a href="https://www.netlify.com/">Netlify</a>.
+		</p>
+
+		<hr class="my-12 border-white/15"/>
+
+		<p>The following sounds are used, all of which are licensed through Creative Commons:</p>
 		<ul class="ml-4 my-8">
 			{#each Object.values(sounds) as sound}
 				<li><a href={sound.source}>{sound.originalName}</a> {@html sound.attribution}.</li>
